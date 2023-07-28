5152fe56ba907ca4097bc7498d92707b34dd63c1
Abhinav Saraswat <abhinavsaraswat@Abhinavs-MacBook-Pro-6.local>
2023-07-28 13:41:00 +0530

    update sidekick

--- tools/quick-publish/src/sharepoint.js
+++ tools/quick-publish/src/sharepoint.js
@@ -1,7 +1,6 @@
 import { PublicClientApplication } from './msal-browser-2.14.2.js';
 import { Document, Paragraph, Packer, HeadingLevel } from 'docx';
 import { saveAs } from 'file-saver';
-import { sendMultipartRequest } from './firefly.js';
 
 const graphURL = 'https://graph.microsoft.com/v1.0';
 const baseURI = 'https://graph.microsoft.com/v1.0/drives/b!9IXcorzxfUm_iSmlbQUd2rvx8XA-4zBAvR2Geq4Y2sZTr_1zgLOtRKRA81cvIhG1/root:/fcbayern';
@@ -95,16 +94,7 @@ export async function PublishAndNotify() {
     // if (quickPublish === 'published') {
     //     return 'updated';
     // }
-    //const folderId = await getFolderID();
-
-    //console.log("Folder Id = " + folderId);
-
-    //uploadDocumentFile(folderID);
-
-    var data = sendMultipartRequest('');
-    if(data) {
-        console.log(data.size());
-    }
+    await uploadDocumentFile(folderID);
 }
 
 
