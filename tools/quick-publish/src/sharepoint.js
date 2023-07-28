92c800d5a63ecb56acc5361236d93d24fc7d3594
Abhinav Saraswat <abhinavsaraswat@Abhinavs-MacBook-Pro-6.local>
2023-07-28 15:07:33 +0530

    update sidekick

--- tools/quick-publish/src/sharepoint.js
+++ tools/quick-publish/src/sharepoint.js
@@ -3,6 +3,7 @@ import { Document, Paragraph, Packer, HeadingLevel } from 'docx';
 import { saveAs } from 'file-saver';
 
 import axios from "axios";
+import FormData from 'form-data';
 
 const graphURL = 'https://graph.microsoft.com/v1.0';
 const baseURI = 'https://graph.microsoft.com/v1.0/drives/b!9IXcorzxfUm_iSmlbQUd2rvx8XA-4zBAvR2Geq4Y2sZTr_1zgLOtRKRA81cvIhG1/root:/fcbayern';
